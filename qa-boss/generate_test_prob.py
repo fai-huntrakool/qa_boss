c8fec6d1422d05c347e4af7d103ffa039c213196
fai-huntrakool <p.huntrakool@gmail.com>
2019-10-25 18:07:40 +0900

    add multiple_ratios

--- qa-boss/generate_test_prob.py
+++ qa-boss/generate_test_prob.py
@@ -15,10 +15,6 @@ def generate_test_case(size, num_terms, var_type="BINARY"):
     return x, numerator, divisor
 
 
-def initialize_lambda(num_terms):
-    return np.random.randint(1, 5, size=num_terms)
-
-
 def initialize_solution(size):
     return np.random.randint(0, 2, size=size)
 
@@ -28,14 +24,4 @@ def construct_bqm(x, lamb, numerator, divisor):
     model = feed_obj_fun.compile()
     bqm = model.to_dimod_bqm()
     return bqm
-
-
-if __name__ == '__main__':
-    size = 15
-    num_terms = 1
-    x, numerator, divisor = generate_test_case(size, num_terms)
-    lamb = initialize_lambda(num_terms)
-    previous_solution = initialize_solution(size)
-    dinkelbach.dinkelbach_for_one_ratio(x, lamb, numerator, divisor, previous_solution)
-    print(solver.exact_solver(numerator, divisor, size, num_terms))
 
--- qa-boss/main.py
+++ qa-boss/main.py
@@ -1,4 +1,4 @@
-import dinkelbach
+import dinkelbach as dkb
 import generate_test_prob as gtp
 import solver
 
@@ -6,12 +6,28 @@ import solver
 def main():
     size = 15
     num_terms = 1
+
     x, numerator, divisor = gtp.generate_test_case(size, num_terms)
-    lamb = gtp.initialize_lambda(num_terms)
+
+    lamb, _, _, _, _, _ = dkb.initialize_lambda(num_terms, size, numerator, divisor)
     previous_solution = gtp.initialize_solution(size)
-    dinkelbach.dinkelbach_for_one_ratio(x, lamb, numerator, divisor, previous_solution)
+
+    dkb.dinkelbach_for_one_ratio(x, lamb, numerator, divisor, previous_solution)
+    print('Exact Solver : ' )
+    print(solver.exact_solver(numerator, divisor, size, num_terms))
+
+def multiple_ratio():
+    size = 3
+    num_terms = 3
+
+    x, numerator, divisor = gtp.generate_test_case(size, num_terms)
+
+    lamb, v, u, vk, uk, previous_solution = dkb.initialize_lambda(num_terms, size, numerator, divisor)
+
+    dkb.dinkelbach_for_multiple_ratios(x, lamb, u, v, numerator, divisor, previous_solution)
+    print('Exact Solver : ')
     print(solver.exact_solver(numerator, divisor, size, num_terms))
 
 
 if __name__ == '__main__':
-    main()
+    multiple_ratio()
